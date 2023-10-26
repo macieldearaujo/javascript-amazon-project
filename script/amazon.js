eb0e347ccd5e4685a833402000b7f4be926b226a
douglasmcl <douglasmacielaraujo@outlook.com>
2023-10-26 13:48:52 -0300

    Display "add to cart" when you click the button

--- script/amazon.js
+++ script/amazon.js
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -86,5 +86,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
       console.log(cart);
+
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      addedMessage.classList.add('added-to-cart-visible');
     })
 })
