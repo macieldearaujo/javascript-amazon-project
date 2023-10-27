a5c375a72572692539019957c75dee3b15e58473
douglasmcl <douglasmacielaraujo@outlook.com>
2023-10-27 14:43:15 -0300

    move addToCart into cart.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart ,  addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -56,32 +56,11 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function addToCart(productId) {
-  let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    })
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-    const quantity = Number(quantitySelector.value);
-
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId,
-        quantity
-      })
-    }
-}
 function updateCartQuantity() {
   let cartQuantity = 0;
 
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
     })
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
@@ -96,7 +75,6 @@ function displayMessage(productId) {
 
   addedMessage.classList.add('added-to-cart-visible');
 
-  /* Timeout "add to cart" message */
   let addedMessageTimeoutId;
 
   if (addedMessageTimeoutId) {
